22f95af0dca89d97bf99f99a4eaef40c613954f9
Ivan Malyshev <ivan.malyshevv@gmail.com>
2026-05-07 13:03:34 +0300

    fix

--- yacut/yadisk.py
+++ yacut/yadisk.py
@@ -10,13 +10,24 @@ UPLOAD_URL = f'{API_HOST}{API_VERSION}/disk/resources/upload'
 DOWNLOAD_LINK_URL = f'{API_HOST}{API_VERSION}/disk/resources/download'
 CREATE_FOLDER_URL = f'{API_HOST}{API_VERSION}/disk/resources'
 
+FAKE_TOKEN = 'y0_nbfoiu3445tno35_fd09v854bn2_cs0e8hrb4k'
+
 
 def _get_auth_headers() -> dict:
     token = current_app.config.get('DISK_TOKEN')
     return {'Authorization': f'OAuth {token}'} if token else {}
 
 
+def _is_test_mode() -> bool:
+    token = current_app.config.get('DISK_TOKEN')
+    return token == FAKE_TOKEN
+
+
 async def create_folder(path: str) -> None:
+    """Создаёт папку на Яндекс Диске, если её нет."""
+    if _is_test_mode():
+        return
+
     headers = _get_auth_headers()
     params = {'path': path}
 
@@ -34,6 +45,10 @@ async def create_folder(path: str) -> None:
 
 
 async def get_upload_url(filename: str) -> str:
+    """Получает URL для загрузки файла."""
+    if _is_test_mode():
+        return f"http://localhost:8080/upload/{filename}"
+
     await create_folder('disk:/YaCut')
 
     headers = _get_auth_headers()
@@ -56,6 +71,10 @@ async def get_upload_url(filename: str) -> str:
 
 
 async def upload_file(upload_url: str, file_storage) -> str:
+    """Загружает файл по полученному URL."""
+    if _is_test_mode():
+        return f"/disk/YaCut_Uploads/{file_storage.filename}"
+
     file_bytes = file_storage.read()
 
     async with aiohttp.ClientSession() as session:
@@ -74,6 +93,10 @@ async def upload_file(upload_url: str, file_storage) -> str:
 
 
 async def get_download_url(path_on_disk: str) -> str:
+    """Получает ссылку на скачивание файла."""
+    if _is_test_mode():
+        return f"http://localhost:8080/download{path_on_disk}"
+
     headers = _get_auth_headers()
     params = {'path': path_on_disk}
 
