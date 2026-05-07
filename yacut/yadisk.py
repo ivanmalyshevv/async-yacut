5782b78dfa87160ec5a94bf1c9e39975de901792
Ivan Malyshev <ivan.malyshevv@gmail.com>
2026-05-07 13:09:14 +0300

    fix

--- yacut/yadisk.py
+++ yacut/yadisk.py
@@ -8,7 +8,6 @@ API_VERSION = 'v1'
 
 UPLOAD_URL = f'{API_HOST}{API_VERSION}/disk/resources/upload'
 DOWNLOAD_LINK_URL = f'{API_HOST}{API_VERSION}/disk/resources/download'
-CREATE_FOLDER_URL = f'{API_HOST}{API_VERSION}/disk/resources'
 
 
 def _get_auth_headers() -> dict:
@@ -16,31 +15,11 @@ def _get_auth_headers() -> dict:
     return {'Authorization': f'OAuth {token}'} if token else {}
 
 
-async def create_folder(path: str) -> None:
-    """Создаёт папку на Яндекс Диске, если её нет."""
-    headers = _get_auth_headers()
-    params = {'path': path}
-
-    async with aiohttp.ClientSession() as session:
-        async with session.put(
-            CREATE_FOLDER_URL, headers=headers, params=params
-        ) as response:
-            if response.status == 409:
-                pass
-            elif response.status not in (200, 201):
-                error_text = await response.text()
-                raise Exception(
-                    f"Ошибка создания папки: {response.status}, {error_text}"
-                )
-
-
 async def get_upload_url(filename: str) -> str:
     """Получает URL для загрузки файла."""
-    await create_folder('disk:/YaCut')
-
     headers = _get_auth_headers()
     params = {
-        'path': f'disk:/YaCut/{filename}',
+        'path': f'disk:/{filename}',
         'overwrite': 'True',
     }
 
